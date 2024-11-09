0f2cbde3efafa7d322811b79ee7a589d4df17d60
camri923 <camri923@student.liu.se>
2024-11-09 15:51:35 +0100

    runfile for app

---
+++ koduppg_CGI/run_api.py
@@ -0,0 +1,13 @@
+""" 
+    This is the script that runs the FastAPI application using Uvicorn,
+    and reloads the app when changes are made.
+"""
+
+import uvicorn
+
+# Start running the uvicorn server and set up the app for incoming HTTP requests on port 3000
+def main():
+    uvicorn.run("main_app:app", port=3000, reload=True) # reload app when changes are made
+
+if __name__ == "__main__":
+    main()
